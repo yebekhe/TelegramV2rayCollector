6626216991cb0a483ef073390dbbca8221662452
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-19 01:53:13 +0330

    Update config.php

--- modules/config.php
+++ modules/config.php
@@ -2,7 +2,31 @@
 error_reporting(0);
 date_default_timezone_set("Asia/Tehran");
 
-$Channel = ["V2rayNGn", "free4allVPN", "PrivateVPNs", "DirectVPN", "ProxyFn", "v2ray_outlineir", "NetAccount", "oneclickvpnkeys", "daorzadannet", "Outline_Vpn", "vpn_xw", "prrofile_purple", "ShadowSocks_s", "azadi_az_inja_migzare", "WomanLifeFreedomVPN", "internet4iran", "LegenderY_Servers" , "vpnfail_v2ray", "UnlimitedDev", "vmessorg", "v2rayNG_Matsuri", "v2rayngvpn", "vpn_ioss"];
+$Channel = [
+    "V2rayNGn",
+    "free4allVPN",
+    "PrivateVPNs",
+    "DirectVPN",
+    "ProxyFn",
+    "v2ray_outlineir",
+    "NetAccount",
+    "oneclickvpnkeys",
+    "daorzadannet",
+    "Outline_Vpn",
+    "vpn_xw",
+    "prrofile_purple",
+    "ShadowSocks_s",
+    "azadi_az_inja_migzare",
+    "WomanLifeFreedomVPN",
+    "internet4iran",
+    "LegenderY_Servers",
+    "vpnfail_v2ray",
+    "UnlimitedDev",
+    "vmessorg",
+    "v2rayNG_Matsuri",
+    "v2rayngvpn",
+    "vpn_ioss",
+];
 $Types = [
     "V2rayNGn" => ["vmess", "vless"],
     "free4allVPN" => ["vmess", "ss"],
@@ -25,9 +49,9 @@ $Types = [
     "vpnfail_v2ray" => ["vmess"],
     "UnlimitedDev" => ["vless"],
     "vmessorg" => ["vmess", "vless"],
-    "v2rayNG_Matsuri" => ["vless"]
+    "v2rayNG_Matsuri" => ["vless"],
     "v2rayngvpn" => ["vless"],
-    "vpn_ioss" => ["vmess"]
+    "vpn_ioss" => ["vmess"],
 ];
 
 ?>
