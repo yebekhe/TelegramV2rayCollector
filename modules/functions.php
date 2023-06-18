eb5adde70e4db248f75ede51187315819c1c60ae
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-18 18:49:52 +0330

    Update functions.php

--- modules/functions.php
+++ modules/functions.php
@@ -1,20 +1,8 @@
 <?php
 include "flag.php";
 include "ipinfo.php";
-
-function decode_vmess($vmess_config)
-{
-    $vmess_data = substr($vmess_config, 8); // remove "vmess://"
-    $decoded_data = json_decode(base64_decode($vmess_data), true);
-    return $decoded_data;
-}
-
-function encode_vmess($config)
-{
-    $encoded_data = base64_encode(json_encode($config));
-    $vmess_config = "vmess://" . $encoded_data;
-    return $vmess_config;
-}
+include "xray.php";
+include "vmess.php";
 
 function ParseShadowsocks($config_str)
 {
