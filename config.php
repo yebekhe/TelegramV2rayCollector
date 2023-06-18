5a2de3dd3b766cb380c92364cf17550ecc445c24
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-18 17:33:18 +0330

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,14 @@
 error_reporting(0);
 date_default_timezone_set("Asia/Tehran");
 
-$Channel = ["V2rayNGn" , ];
+$Channel = ["V2rayNGn", "free4allVPN", "PrivateVPNs", "DirectVPN", "ProxyFn", "v2ray_outlineir"];
 $Types = [
-    "V2rayNGn" => ["vm", "vl"],
+    "V2rayNGn" => ["vmess", "vless"],
+    "free4allVPN" => ["vmess", "ss"],
+    "PrivateVPNs" => ["trojan"],
+    "DirectVPN" => ["trojan"],
+    "ProxyFn" => ["vless"],
+    "v2ray_outlineir" => ["vmess"],
 ];
 
 ?>
