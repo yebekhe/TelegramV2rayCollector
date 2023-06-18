a594b0117f1c5e2e612b6bd970876674e8be5b01
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-19 01:21:18 +0330

    Update config.php

--- modules/config.php
+++ modules/config.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 date_default_timezone_set("Asia/Tehran");
 
-$Channel = ["V2rayNGn", "free4allVPN", "PrivateVPNs", "DirectVPN", "ProxyFn", "v2ray_outlineir"];
+$Channel = ["V2rayNGn", "free4allVPN", "PrivateVPNs", "DirectVPN", "ProxyFn", "v2ray_outlineir", "NetAccount", "oneclickvpnkeys", "daorzadannet", "Outline_Vpn", "vpn_xw", "prrofile_purple", "ShadowSocks_s"];
 $Types = [
     "V2rayNGn" => ["vmess", "vless"],
     "free4allVPN" => ["vmess", "ss"],
@@ -10,6 +10,14 @@ $Types = [
     "DirectVPN" => ["trojan"],
     "ProxyFn" => ["vless"],
     "v2ray_outlineir" => ["vmess"],
+    "NetAccount" => ["trojan"],
+    "oneclickvpnkeys" => ["vmess", "trojan"],
+    "daorzadannet" => ["vmess", "vless"],
+    "Outline_Vpn" => ["vless"],
+    "vpn_xw" => ["vless"],
+    "prrofile_purple" => ["vmess", "vless"],
+    "ShadowSocks_s" => ["vless"]
+    
 ];
 
 ?>
