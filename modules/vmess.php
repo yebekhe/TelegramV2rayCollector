8fb0227ab230603ff18161c53c6130fef6b177af
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-18 18:50:10 +0330

    Create vmess.php

---
+++ modules/vmess.php
@@ -0,0 +1,16 @@
+<?php
+function decode_vmess($vmess_config)
+{
+    $vmess_data = substr($vmess_config, 8); // remove "vmess://"
+    $decoded_data = json_decode(base64_decode($vmess_data), true);
+    return $decoded_data;
+}
+
+function encode_vmess($config)
+{
+    $encoded_data = base64_encode(json_encode($config));
+    $vmess_config = "vmess://" . $encoded_data;
+    return $vmess_config;
+}
+
+?>
