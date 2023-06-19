a1c8347ceb3bc812e6b97906c76bd548520b955f
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-19 04:48:07 +0330

    Update index.php

--- index.php
+++ index.php
@@ -9,13 +9,9 @@ include "modules/config.php";
 
 $mix_data = "";
 $vmess_data = "";
-#$vmess_array = [];
 $trojan_data = "";
-#trojan_array = [];
 $vless_data = "";
-#vless_array = [];
 $shadowsocks_data = "";
-#$shadowsocks_array = [];
 
 for ($p = count($Channel) - 1; $p >= 0; $p--) {
     $CH = $Channel[$p];
@@ -27,49 +23,33 @@ for ($p = count($Channel) - 1; $p >= 0; $p--) {
         if ($Types[$CH][$type_count] === "vmess"){
             if ($vmess_data === ""){
                 $vmess_data = get_v2ray($CH, $Types[$CH][$type_count] , "text");
-                #$vmess_array = get_v2ray($CH, $Types[$CH][$type_count] , "json");
             }
             else{
                 $vmess_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count] , "text");
-                #$vmess_temp_array = get_v2ray($CH, $Types[$CH][$type_count] , "json");
-                #$result = array_merge($vmess_array, $vmess_temp_array);
-                #$vmess_array = $result;
             }
         }
         elseif ($Types[$CH][$type_count] === "vless"){
             if ($vless_data === ""){
                 $vless_data = get_v2ray($CH, $Types[$CH][$type_count] , "text");
-                #$vless_array = get_v2ray($CH, $Types[$CH][$type_count] , "json");
             }
             else{
                 $vless_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count] , "text");
-                #$vless_temp_array = get_v2ray($CH, $Types[$CH][$type_count] , "json");
-                #$result = array_merge($vless_array, $vless_temp_array);
-                #$vless_array = $result;
             }
         }
         elseif ($Types[$CH][$type_count] === "trojan"){
             if ($trojan_data === ""){
                 $trojan_data = get_v2ray($CH, $Types[$CH][$type_count] , "text");
-                #$trojan_array = get_v2ray($CH, $Types[$CH][$type_count] , "json");
             }
             else{
                 $trojan_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count] , "text");
-                #$trojan_temp_array = get_v2ray($CH, $Types[$CH][$type_count] , "json");
-                #$result = array_merge($trojan_array, $trojan_temp_array);
-                #$trojan_array = $result;
             }
         }
         elseif ($Types[$CH][$type_count] === "ss"){
             if ($shadowsocks_data === ""){
                 $shadowsocks_data = get_v2ray($CH, $Types[$CH][$type_count] , "text");
-                #$shadowsocks_array = get_v2ray($CH, $Types[$CH][$type_count] , "json");
             }
             else{
                 $shadowsocks_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count] , "text");
-                #$shadowsocks_temp_array = get_v2ray($CH, $Types[$CH][$type_count] , "json");
-                #$result = array_merge($shadowsocks_array, $shadowsocks_temp_array);
-                #$shadowsocks_array = $result;
             }
         }
 
