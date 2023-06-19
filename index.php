bbd5e9d330e2d27e904affc52231c25630b12f3d
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-19 05:14:59 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,58 +1,10 @@
 <?php
 
+error_reporting(0);
 header("Content-type: application/json;");
 
 include "modules/getv2ray.php";
 include "modules/config.php";
-
-$mix_data = "";
-$vmess_data = "";
-$trojan_data = "";
-$vless_data = "";
-$shadowsocks_data = "";
-
-for ($p = count($Channel) - 1; $p >= 0; $p--) {
-    $CH = $Channel[$p];
-    for (
-        $type_count = count($Types[$CH]) - 1;
-        $type_count >= 0;
-        $type_count--
-    ) {
-        if ($Types[$CH][$type_count] === "vmess") {
-            if ($vmess_data === "") {
-                $vmess_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
-            } else {
-                $vmess_data .=
-                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
-            }
-        } elseif ($Types[$CH][$type_count] === "vless") {
-            if ($vless_data === "") {
-                $vless_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
-            } else {
-                $vless_data .=
-                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
-            }
-        } elseif ($Types[$CH][$type_count] === "trojan") {
-            if ($trojan_data === "") {
-                $trojan_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
-            } else {
-                $trojan_data .=
-                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
-            }
-        } elseif ($Types[$CH][$type_count] === "ss") {
-            if ($shadowsocks_data === "") {
-                $shadowsocks_data = get_v2ray(
-                    $CH,
-                    $Types[$CH][$type_count],
-                    "text"
-                );
-            } else {
-                $shadowsocks_data .=
-                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
-            }
-        }
-    }
-}
 
 function remove_duplicate_vmess($input)
 {
@@ -96,6 +48,51 @@ function remove_duplicate_non_vmess($input)
         $output .= $output == "" ? $config : "\n" . $config;
     }
     return $output;
+}
+
+$mix_data = "";
+$vmess_data = "";
+$trojan_data = "";
+$vless_data = "";
+$shadowsocks_data = "";
+
+for ($p = count($Channel) - 1; $p >= 0; $p--) {
+    $CH = $Channel[$p];
+    for (
+        $type_count = count($Types[$CH]) - 1;
+        $type_count >= 0;
+        $type_count--
+    ) {
+        if ($Types[$CH][$type_count] === "vmess") {
+            if ($vmess_data === "") {
+                $vmess_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
+            } else {
+                $vmess_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
+            }
+        } elseif ($Types[$CH][$type_count] === "vless") {
+            if ($vless_data === "") {
+                $vless_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
+            } else {
+                $vless_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
+            }
+        } elseif ($Types[$CH][$type_count] === "trojan") {
+            if ($trojan_data === "") {
+                $trojan_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
+            } else {
+                $trojan_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
+            }
+        } elseif ($Types[$CH][$type_count] === "ss") {
+            if ($shadowsocks_data === "") {
+                $shadowsocks_data = get_v2ray(
+                    $CH,
+                    $Types[$CH][$type_count],
+                    "text"
+                );
+            } else {
+                $shadowsocks_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
+            }
+        }
+    }
 }
 
 $fixed_string_vmess = remove_duplicate_vmess($vmess_data);
