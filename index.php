55e7f4c30e96ebbdf18872a66f5dff561bbc78cc
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-19 04:54:40 +0330

    Update index.php

--- index.php
+++ index.php
@@ -20,56 +20,57 @@ for ($p = count($Channel) - 1; $p >= 0; $p--) {
         $type_count >= 0;
         $type_count--
     ) {
-        if ($Types[$CH][$type_count] === "vmess"){
-            if ($vmess_data === ""){
-                $vmess_data = get_v2ray($CH, $Types[$CH][$type_count] , "text");
+        if ($Types[$CH][$type_count] === "vmess") {
+            if ($vmess_data === "") {
+                $vmess_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
+            } else {
+                $vmess_data .=
+                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
             }
-            else{
-                $vmess_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count] , "text");
+        } elseif ($Types[$CH][$type_count] === "vless") {
+            if ($vless_data === "") {
+                $vless_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
+            } else {
+                $vless_data .=
+                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
+            }
+        } elseif ($Types[$CH][$type_count] === "trojan") {
+            if ($trojan_data === "") {
+                $trojan_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
+            } else {
+                $trojan_data .=
+                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
+            }
+        } elseif ($Types[$CH][$type_count] === "ss") {
+            if ($shadowsocks_data === "") {
+                $shadowsocks_data = get_v2ray(
+                    $CH,
+                    $Types[$CH][$type_count],
+                    "text"
+                );
+            } else {
+                $shadowsocks_data .=
+                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
             }
         }
-        elseif ($Types[$CH][$type_count] === "vless"){
-            if ($vless_data === ""){
-                $vless_data = get_v2ray($CH, $Types[$CH][$type_count] , "text");
-            }
-            else{
-                $vless_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count] , "text");
-            }
-        }
-        elseif ($Types[$CH][$type_count] === "trojan"){
-            if ($trojan_data === ""){
-                $trojan_data = get_v2ray($CH, $Types[$CH][$type_count] , "text");
-            }
-            else{
-                $trojan_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count] , "text");
-            }
-        }
-        elseif ($Types[$CH][$type_count] === "ss"){
-            if ($shadowsocks_data === ""){
-                $shadowsocks_data = get_v2ray($CH, $Types[$CH][$type_count] , "text");
-            }
-            else{
-                $shadowsocks_data .= "\n" . get_v2ray($CH, $Types[$CH][$type_count] , "text");
-            }
-        }
-
     }
 }
 
-function remove_duplicate_vmess($input){
+function remove_duplicate_vmess($input)
+{
     $array = explode("\n", $input);
     foreach ($array as $item) {
         $parts = decode_vmess($item);
-        $part_ps = $parts['ps'];
-        unset($parts['ps']);
+        $part_ps = $parts["ps"];
+        unset($parts["ps"]);
         $part_serialize = serialize($parts);
-        $result[$part_serialize][] = $part_ps ?? '';
+        $result[$part_serialize][] = $part_ps ?? "";
     }
     $finalResult = [];
     foreach ($result as $serial => $ps) {
-        $partAfterHash = $ps[0] ?? '';
+        $partAfterHash = $ps[0] ?? "";
         $part_serialize = unserialize($serial);
-        $part_serialize['ps'] = $partAfterHash;
+        $part_serialize["ps"] = $partAfterHash;
         $finalResult[] = encode_vmess($part_serialize);
     }
     foreach ($finalResult as $config) {
@@ -78,17 +79,18 @@ function remove_duplicate_vmess($input){
     return $output;
 }
 
-function remove_duplicate_non_vmess($input){
+function remove_duplicate_non_vmess($input)
+{
     $array = explode("\n", $input);
 
     foreach ($array as $item) {
         $parts = explode("#", $item);
-        $result[$parts[0]][] = $parts[1] ?? '';
+        $result[$parts[0]][] = $parts[1] ?? "";
     }
     $finalResult = [];
     foreach ($result as $domain => $parts) {
-        $partAfterHash = $parts[0] ?? '';
-        $finalResult[] = $domain . '#' . $partAfterHash;
+        $partAfterHash = $parts[0] ?? "";
+        $finalResult[] = $domain . "#" . $partAfterHash;
     }
     foreach ($finalResult as $config) {
         $output .= $output == "" ? $config : "\n" . $config;
@@ -97,20 +99,34 @@ function remove_duplicate_non_vmess($input){
 }
 
 $fixed_string_vmess = remove_duplicate_vmess($vmess_data);
-$fixed_string_vless = remove_duplicate_non_vmess(str_replace("&amp;", "&", $vless_data));
-$fixed_string_trojan = remove_duplicate_non_vmess(str_replace("&amp;", "&", $trojan_data));
-$fixed_string_$shadowsocks = remove_duplicate_non_vmess($shadowsocks_data);
+$fixed_string_vless = remove_duplicate_non_vmess(
+    str_replace("&amp;", "&", $vless_data)
+);
+$fixed_string_trojan = remove_duplicate_non_vmess(
+    str_replace("&amp;", "&", $trojan_data)
+);
+$fixed_string_shadowsocks = remove_duplicate_non_vmess($shadowsocks_data);
 
-$mix_data = $fixed_string_vmess . "\n" . $fixed_string_vless . "\n" . $fixed_string_trojan . "\n" . $fixed_string_$shadowsocks;
+$mix_data =
+    $fixed_string_vmess .
+    "\n" .
+    $fixed_string_vless .
+    "\n" .
+    $fixed_string_trojan .
+    "\n" .
+    $fixed_string_shadowsocks;
 
-file_put_contents("sub/mix" , $mix_data);
+file_put_contents("sub/mix", $mix_data);
 file_put_contents("sub/vmess", $fixed_string_vmess);
 file_put_contents("sub/vless", $fixed_string_vless);
 file_put_contents("sub/trojan", $fixed_string_trojan);
-file_put_contents("sub/shadowsocks", $fixed_string_$shadowsocks);
-file_put_contents("sub/mix_base64" , base64_encode($mix_data));
+file_put_contents("sub/shadowsocks", $fixed_string_shadowsocks);
+file_put_contents("sub/mix_base64", base64_encode($mix_data));
 file_put_contents("sub/vmess_base64", base64_encode($fixed_string_vmess));
 file_put_contents("sub/vless_base64", base64_encode($fixed_string_vless));
 file_put_contents("sub/trojan_base64", base64_encode($fixed_string_trojan));
-file_put_contents("sub/shadowsocks_base64", base64_encode($fixed_string_$shadowsocks));
+file_put_contents(
+    "sub/shadowsocks_base64",
+    base64_encode($fixed_string_shadowsocks)
+);
 ?>
