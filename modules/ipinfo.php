4d83709526ae311cbea327aa3581bc12e9f0989e
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-21 01:41:36 +0330

    Update ipinfo.php

--- modules/ipinfo.php
+++ modules/ipinfo.php
@@ -1,41 +1,28 @@
 <?php
 
-function is_ip($string) {
-  $ip_pattern = '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/';
-  if (preg_match($ip_pattern, $string)) {
-    return true;
-  } else {
-    return false;
-  }
+function is_ip($string)
+{
+    $ip_pattern = '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/';
+    if (preg_match($ip_pattern, $string)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function ip_info($ip)
 {
     if (is_ip($ip) === false) {
-        $ip_address_array = dns_get_record($ip,  DNS_A);
+        $ip_address_array = dns_get_record($ip, DNS_A);
         $randomKey = array_rand($ip_address_array);
-        $ip = $ip_address_array[$randomKey]['ip'];
+        $ip = $ip_address_array[$randomKey]["ip"];
     }
-    // Initialize cURL
-    $ch = curl_init();
-
-    // Set cURL options
-    curl_setopt($ch, CURLOPT_URL, 'https://api.country.is/' . $ip);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    // Send the HTTP request and get the response
-    $response = curl_exec($ch);
-
-    // Check for errors
-    if (curl_error($ch)) {
-        echo 'cURL error: ' . curl_error($ch);
-    }
-
-    // Close the cURL session
-    curl_close($ch);
-
-    // Print the response
-    return $response;
+    $ipinfo = json_decode(
+        file_get_contents("https://api.country.is/" . $ip),
+        true
+    );
+    return $ipinfo;
 }
 
 ?>
+
