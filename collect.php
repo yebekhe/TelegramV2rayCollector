39db4ce36bdb1fad68278bcb3225cf4befad79ac
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-22 02:49:54 +0330

    Update collect.php

--- collect.php
+++ collect.php
@@ -18,11 +18,17 @@ function get_reality($input)
     return $output;
 }
 
-$mix_data = "";
+$mix = "";
 $vmess_data = "";
 $trojan_data = "";
 $vless_data = "";
 $shadowsocks_data = "";
+
+$mix_data = [];
+$vmess_array = [];
+$vless_array = [];
+$trojan_array = [];
+$shadowsocks_array = [];
 
 for ($p = count($Channel) - 1; $p >= 0; $p--) {
     $CH = $Channel[$p];
@@ -32,50 +38,57 @@ for ($p = count($Channel) - 1; $p >= 0; $p--) {
         $type_count--
     ) {
         if ($Types[$CH][$type_count] === "vmess") {
-            if ($vmess_data === "") {
-                $vmess_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
-            } else {
-                $vmess_data .=
-                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
-            }
+            $vmess_data = array_merge(
+                $vmess_data,
+                get_config($CH, $Types[$CH][$type_count])
+            );
         } elseif ($Types[$CH][$type_count] === "vless") {
-            if ($vless_data === "") {
-                $vless_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
-            } else {
-                $vless_data .=
-                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
-            }
+            $vless_data = array_merge(
+                $vless_data,
+                get_config($CH, $Types[$CH][$type_count])
+            );
         } elseif ($Types[$CH][$type_count] === "trojan") {
-            if ($trojan_data === "") {
-                $trojan_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
-            } else {
-                $trojan_data .=
-                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
-            }
+            $trojan_data = array_merge(
+                $trojan_data,
+                get_config($CH, $Types[$CH][$type_count])
+            );
         } elseif ($Types[$CH][$type_count] === "ss") {
-            if ($shadowsocks_data === "") {
-                $shadowsocks_data = get_v2ray(
-                    $CH,
-                    $Types[$CH][$type_count],
-                    "text"
-                );
-            } else {
-                $shadowsocks_data .=
-                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
-            }
+            $shadowsocks_data = array_merge(
+                $shadowsocks_data,
+                get_config($CH, $Types[$CH][$type_count])
+            );
         }
     }
 }
 
-$fixed_string_vmess = remove_duplicate_vmess($vmess_data);
-$string_vless = str_replace("&amp;", "&", $vless_data);
+
+foreach ($vmess_data as $object) {
+    $vmess_array[] = $object['config'];
+}
+foreach ($vless_data as $object) {
+    $vless_array[] = $object['config'];
+}
+foreach ($trojan_data as $object) {
+    $trojan_array[] = $object['config'];
+}
+foreach ($shadowsocks_data as $object) {
+    $shadowsocks_array[] = $object['config'];
+}
+
+$vmess = implode("\n", $vmess_array);
+$vless = implode("\n", $vless_array);
+$trojan = implode("\n", $trojan_array);
+$shadowsocks = implode("\n", $shadowsocks_array);
+
+$fixed_string_vmess = remove_duplicate_vmess($vmess);
+$string_vless = str_replace("&amp;", "&", $vless);
 $fixed_string_vless = remove_duplicate_xray($string_vless, "vless");
-$string_trojan = str_replace("&amp;", "&", $trojan_data);
+$string_trojan = str_replace("&amp;", "&", $trojan);
 $fixed_string_trojan = remove_duplicate_xray($string_trojan, "trojan");
-$fixed_string_shadowsocks = remove_duplicate_ss($shadowsocks_data);
+$fixed_string_shadowsocks = remove_duplicate_ss($shadowsocks);
 $fixed_string_reality = get_reality($fixed_string_vless);
 
-$mix_data =
+$mix =
     $fixed_string_vmess .
     "\n" .
     $fixed_string_vless .
@@ -84,14 +97,14 @@ $mix_data =
     "\n" .
     $fixed_string_shadowsocks;
 
-file_put_contents("sub/mix", $mix_data);
+file_put_contents("sub/mix", $mix);
 file_put_contents("sub/vmess", $fixed_string_vmess);
 file_put_contents("sub/vless", $fixed_string_vless);
 file_put_contents("sub/reality", $fixed_string_reality);
 file_put_contents("sub/trojan", $fixed_string_trojan);
 file_put_contents("sub/shadowsocks", $fixed_string_shadowsocks);
 
-file_put_contents("sub/mix_base64", base64_encode($mix_data));
+file_put_contents("sub/mix_base64", base64_encode($mix));
 file_put_contents("sub/vmess_base64", base64_encode($fixed_string_vmess));
 file_put_contents("sub/vless_base64", base64_encode($fixed_string_vless));
 file_put_contents("sub/reality_base64", base64_encode($fixed_string_reality));
@@ -103,4 +116,12 @@ file_put_contents("clash/vmess.yml", convert_to_clash("https://raw.githubusercon
 file_put_contents("clash/trojan.yml", convert_to_clash("https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/trojan_base64"));
 file_put_contents("clash/shadowsocks.yml", convert_to_clash("https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/shadowsocks_base64"));
 
+$mix_data = array_merge(
+    $vmess_data,
+    $vless_data,
+    $trojan_data,
+    $shadowsocks_data
+);
+
+file_put_contents("json/configs.json", json_encode($mix_data, JSON_PRETTY_PRINT));
 ?>
