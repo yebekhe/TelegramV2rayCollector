33a0858aa785f30f30638121250a519110dcd7a5
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-06-24 11:58:13 +0330

    Update clash.php

--- modules/clash.php
+++ modules/clash.php
@@ -1,7 +1,8 @@
 <?php
-function convert_to_clash($input)
+function convert_to_clash($input , $type = "clash")
 {
-  $data = file_get_contents("https://pub-api-1.bianyuan.xyz/sub?target=clash&url=" . $input . "&insert=false");
+  if ($type === "clash"){
+  $data = file_get_contents("https://pub-api-1.bianyuan.xyz/sub?target=clash&url=" . $input . "&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online_Mini.ini&emoji=true&list=false&tfo=false&scv=false&fdn=false&sort=false&new_name=true");
   $translations = array(
     '节点选择' => 'MANUAL',
     '自动选择' => 'AUTO SELECT',
@@ -18,7 +19,17 @@ function convert_to_clash($input)
     '全球直连' => 'GLOBAL DIRECT',
     '漏网之鱼' => 'LEAK',
   );
-  $config_en = str_replace(array_keys($translations), array_values($translations), $data);
-  return $config_en;
+  $config = str_replace(array_keys($translations), array_values($translations), $data);
+  return $config;
+  }
+  elseif ($type === "meta"){
+    $data = file_get_contents("https://sub.bonds.id/sub2?target=clash&url=" . $input . "&insert=false&config=base%2Fdatabase%2Fconfig%2Fcustom%2Fgroups%2Fallgroup_redir.ini&emoji=false&list=false&udp=true&tfo=false&expand=false&scv=true&fdn=false&sort=false&new_name=true");
+    $translations = array(
+    'FASTSSH-SSHKIT-HOWDY' => 'MANUAL',
+      );
+    $config = str_replace(array_keys($translations), array_values($translations), $data);
+    return $config;
+  }
 }
+
 ?>
