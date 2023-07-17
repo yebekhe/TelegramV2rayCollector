a4e4bccdc7269edb54e00046cdad3d37870004ac
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-17 03:56:20 +0330

    Update get_data.php

--- modules/get_data.php
+++ modules/get_data.php
@@ -258,4 +258,194 @@ function get_config($channel, $type)
     // Return the final data array
     return $final_data;
 }
+
+function process_subscription($input, $channel){
+    $final_data = [];
+    $configs = explode("\n", $input);
+    $array_helper_vmess = 0;
+    $array_helper_vless = 0;
+    $array_helper_ss = 0;
+    $array_helper_trojan = 0;
+    foreach ($configs as $config){
+        if (substr($config, 0, 8) === "vmess://"){
+                // Decode the vmess configuration
+                $the_config = decode_vmess($config);
+
+                // Extract IP and port from the decoded configuration
+                $ip = !empty($the_config["sni"])
+                    ? $the_config["sni"]
+                    : (!empty($the_config["host"])
+                        ? $the_config["host"]
+                        : $the_config["add"]);
+                $port = $the_config["port"];
+
+                // Ping the IP and port to get the response time
+                @$ping_data = ping($ip, $port);
+
+                // If ping data is available
+                if ($ping_data !== "unavailable") {
+                    // Get IP information (country) and flag
+                    $ip_info = ip_info($ip);
+                    if (isset($ip_info["country"])) {
+                        $location = $ip_info["country"];
+                        $flag = getFlags($location);
+                    } else {
+                        $flag = "🚩";
+                    }
+
+                    // Update the configuration with channel info, flag, and ping data
+                    $the_config["ps"] =
+                        "@" . $channel . "|" . $flag . "|" . $ping_data;
+
+                    // Encode the updated configuration
+                    $final_config = encode_vmess($the_config);
+
+                    // Build the final data array with channel, type, config, and time
+                    $final_data['vmess'][$array_helper_vmess]["channel"]['username'] = $channel;
+                    $final_data['vmess'][$array_helper_vmess]["channel"]['title'] = $channel;
+                    $final_data['vmess'][$array_helper_vmess]["channel"]['logo'] = "null";
+                    $final_data['vmess'][$array_helper_vmess]["type"] = "vmess";
+                    $final_data['vmess'][$array_helper_vmess]["config"] = $final_config;
+                    $final_data['vmess'][$array_helper_vmess]["time"] = tehran_time();
+                    $array_helper_vmess ++ ;
+            }
+        } elseif (substr($config, 0, 8) === "vless://") {
+            // Parse the vless configuration
+            $the_config = parseProxyUrl(
+                $config,
+                "vless"
+            );
+
+            // Extract IP and port from the parsed vless configuration
+            $ip = !empty($the_config["params"]["sni"])
+                ? $the_config["params"]["sni"]
+                : (!empty($the_config["params"]["host"])
+                    ? $the_config["params"]["host"]
+                    : $the_config["hostname"]);
+            $port = $the_config["port"];
+
+            // Ping the IP and port to get the response time
+            @$ping_data = ping($ip, $port);
+
+            // If ping data is available
+            if ($ping_data !== "unavailable") {
+                // Get IP information (country) and flag
+                $ip_info = ip_info($ip);
+                if (isset($ip_info["country"])) {
+                    $location = $ip_info["country"];
+                    $flag = getFlags($location);
+                } else {
+                    $flag = "🚩";
+                }
+
+                // Update the configuration with channel info, flag, and ping data
+                if (stripos($config, "reality") !== false) {
+                    $the_config["hash"] =
+                        "REALITY|" .
+                        "@" .
+                        $channel .
+                        "|" .
+                        $flag .
+                        "|" .
+                        $ping_data;
+                    $type = "reality";
+                } else {
+                    $the_config["hash"] =
+                        "@" . $channel . "|" . $flag . "|" . ping($ip, $port);
+                    $type = "vless";
+                }
+
+                // Build the final vless configuration
+                $final_config = buildProxyUrl($the_config, "vless");
+
+                // Build the final data array with channel, type, config, and time
+                $final_data['vless'][$array_helper_vless]["channel"]['username'] = $channel;
+                $final_data['vless'][$array_helper_vless]["channel"]['title'] = $channel;
+                $final_data['vless'][$array_helper_vless]["channel"]['logo'] = "null";
+                $final_data['vless'][$array_helper_vless]["type"] = $type;
+                $final_data['vless'][$array_helper_vless]["config"] = urldecode($final_config);
+                $final_data['vless'][$array_helper_vless]["time"] = tehran_time();
+                $array_helper_vless ++ ;
+            }
+        } elseif(substr($config, 0, 5) === "ss://") {
+            // Parse the shadowsocks configuration
+            $the_config = ParseShadowsocks($config);
+
+            // Extract IP and port from the parsed shadowsocks configuration
+            $ip = $the_config["server_address"];
+            $port = $the_config["server_port"];
+
+            // Ping the IP and port to get the response time
+            @$ping_data = ping($ip, $port);
+            // If ping data is available
+            if ($ping_data !== "unavailable") {
+                // Get IP information (country) and flag
+                $ip_info = ip_info($ip);
+                if (isset($ip_info["country"])) {
+                    $location = $ip_info["country"];
+                    $flag = getFlags($location);
+                } else {
+                    $flag = "🚩";
+                }
+
+                // Update the configuration with channel info, flag, and ping data
+                $the_config["remarks"] =
+                    "@" . $channel . "|" . $flag . "|" . $ping_data;
+
+                // Build the final shadowsocks configuration
+                $final_config = BuildShadowsocks($the_config);
+
+                // Build the final data array with channel, type, config, and time
+                $final_data['ss'][$array_helper_ss]["channel"]['username'] = $channel;
+                $final_data['ss'][$array_helper_ss]["channel"]['title'] = $channel;
+                $final_data['ss'][$array_helper_ss]["channel"]['logo'] = "null";
+                $final_data['ss'][$array_helper_ss]["type"] = "ss";
+                $final_data['ss'][$array_helper_ss]["config"] = urldecode($final_config);
+                $final_data['ss'][$array_helper_ss]["time"] = tehran_time();
+                $array_helper_ss ++ ;
+            }
+        } elseif (stripos($config, "trojan://")){
+        // Parse the trojan configuration
+        $the_config = parseProxyUrl($config);
+
+        // Extract IP and port from the parsed trojan configuration
+        $ip = !empty($the_config["params"]["sni"])
+            ? $the_config["params"]["sni"]
+            : (!empty($the_config["params"]["host"])
+                ? $the_config["params"]["host"]
+                : $the_config["hostname"]);
+        $port = $the_config["port"];
+
+        // Ping the IP and port to get the response time
+        @$ping_data = ping($ip, $port);
+
+        // If ping data is available
+        if ($ping_data !== "unavailable") {
+            // Get IP information (country) and flag
+            $ip_info = ip_info($ip);
+            if (isset($ip_info["country"])) {
+                $location = $ip_info["country"];
+                $flag = getFlags($location);
+            } else {
+                $flag = "🚩";
+            }
+
+            // Update the configuration with channel info, flag, and ping data
+            $the_config["hash"] =
+                "@" . $channel . "|" . $flag . "|" . $ping_data;
+
+            // Build the final trojan configuration
+            $final_config = buildProxyUrl($the_config);
+
+            // Build the final data array with channel, type, config, and time
+            $final_data['trojan'][$array_helper_trojan]['username'] = $channel;
+            $final_data['trojan'][$array_helper_trojan]["channel"]['title'] = $channel;
+            $final_data['trojan'][$array_helper_trojan]["channel"]['logo'] = 'null';
+            $final_data['trojan'][$array_helper_trojan]["type"] = "trojan";
+            $final_data['trojan'][$array_helper_trojan]["config"] = urldecode($final_config);
+            $final_data['trojan'][$array_helper_trojan]["time"] = tehran_time();
+            $array_helper_trojan ++ ;
+        }
+    }
+}
 ?>
