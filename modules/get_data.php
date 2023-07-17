792144dbd2414510006157361e4a7301839a51e0
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-17 04:39:50 +0330

    Update get_data.php

--- modules/get_data.php
+++ modules/get_data.php
@@ -19,7 +19,10 @@ function get_config($channel, $type)
     $get = file_get_contents("https://t.me/s/" . $channel);
 
     // Load channels_assets JSON data
-    $channels_assets = json_decode(file_get_contents("modules/channels/channels_assets.json"), true);
+    $channels_assets = json_decode(
+        file_get_contents("modules/channels/channels_assets.json"),
+        true
+    );
 
     // Determine the pattern and perform matching based on the type
     if ($type === "vmess") {
@@ -61,7 +64,10 @@ function get_config($channel, $type)
     foreach ($configs[1] as $key => $config) {
         if ($key >= $key_limit) {
             // Check for ellipsis or invalid characters in the config
-            if (stripos($config, "…") !== false or stripos($config, "...") !== false) {
+            if (
+                stripos($config, "…") !== false or
+                stripos($config, "...") !== false
+            ) {
                 null;
             } else {
                 // Process the configuration based on the type
@@ -102,9 +108,11 @@ function get_config($channel, $type)
                         $final_config = encode_vmess($the_config);
 
                         // Build the final data array with channel, type, config, and time
-                        $final_data[$key]["channel"]['username'] = $channel;
-                        $final_data[$key]["channel"]['title'] = $channels_assets[$channel]['title'];
-                        $final_data[$key]["channel"]['logo'] = $channels_assets[$channel]['logo'];
+                        $final_data[$key]["channel"]["username"] = $channel;
+                        $final_data[$key]["channel"]["title"] =
+                            $channels_assets[$channel]["title"];
+                        $final_data[$key]["channel"]["logo"] =
+                            $channels_assets[$channel]["logo"];
                         $final_data[$key]["type"] = $type;
                         $final_data[$key]["config"] = $final_config;
                         $final_data[$key]["time"] = convert_to_iran_time(
@@ -153,16 +161,23 @@ function get_config($channel, $type)
                             $type = "reality";
                         } else {
                             $the_config["hash"] =
-                                "@" . $channel . "|" . $flag . "|" . ping($ip, $port);
+                                "@" .
+                                $channel .
+                                "|" .
+                                $flag .
+                                "|" .
+                                ping($ip, $port);
                         }
 
                         // Build the final vless configuration
                         $final_config = buildProxyUrl($the_config, "vless");
 
                         // Build the final data array with channel, type, config, and time
-                        $final_data[$key]["channel"]['username'] = $channel;
-                        $final_data[$key]["channel"]['title'] = $channels_assets[$channel]['title'];
-                        $final_data[$key]["channel"]['logo'] = $channels_assets[$channel]['logo'];
+                        $final_data[$key]["channel"]["username"] = $channel;
+                        $final_data[$key]["channel"]["title"] =
+                            $channels_assets[$channel]["title"];
+                        $final_data[$key]["channel"]["logo"] =
+                            $channels_assets[$channel]["logo"];
                         $final_data[$key]["type"] = $type;
                         $final_data[$key]["config"] = urldecode($final_config);
                         $final_data[$key]["time"] = convert_to_iran_time(
@@ -203,9 +218,11 @@ function get_config($channel, $type)
                         $final_config = buildProxyUrl($the_config);
 
                         // Build the final data array with channel, type, config, and time
-                        $final_data[$key]["channel"]['username'] = $channel;
-                        $final_data[$key]["channel"]['title'] = $channels_assets[$channel]['title'];
-                        $final_data[$key]["channel"]['logo'] = $channels_assets[$channel]['logo'];
+                        $final_data[$key]["channel"]["username"] = $channel;
+                        $final_data[$key]["channel"]["title"] =
+                            $channels_assets[$channel]["title"];
+                        $final_data[$key]["channel"]["logo"] =
+                            $channels_assets[$channel]["logo"];
                         $final_data[$key]["type"] = $type;
                         $final_data[$key]["config"] = urldecode($final_config);
                         $final_data[$key]["time"] = convert_to_iran_time(
@@ -241,9 +258,11 @@ function get_config($channel, $type)
                         $final_config = BuildShadowsocks($the_config);
 
                         // Build the final data array with channel, type, config, and time
-                        $final_data[$key]["channel"]['username'] = $channel;
-                        $final_data[$key]["channel"]['title'] = $channels_assets[$channel]['title'];
-                        $final_data[$key]["channel"]['logo'] = $channels_assets[$channel]['logo'];
+                        $final_data[$key]["channel"]["username"] = $channel;
+                        $final_data[$key]["channel"]["title"] =
+                            $channels_assets[$channel]["title"];
+                        $final_data[$key]["channel"]["logo"] =
+                            $channels_assets[$channel]["logo"];
                         $final_data[$key]["type"] = $type;
                         $final_data[$key]["config"] = urldecode($final_config);
                         $final_data[$key]["time"] = convert_to_iran_time(
@@ -259,62 +278,69 @@ function get_config($channel, $type)
     return $final_data;
 }
 
-function process_subscription($input, $channel){
+function process_subscription($input, $channel)
+{
     $final_data = [];
     $configs = explode("\n", $input);
     $array_helper_vmess = 0;
     $array_helper_vless = 0;
     $array_helper_ss = 0;
     $array_helper_trojan = 0;
-    foreach ($configs as $config){
-        if (substr($config, 0, 8) === "vmess://"){
-                // Decode the vmess configuration
-                $the_config = decode_vmess($config);
+    foreach ($configs as $config) {
+        if (substr($config, 0, 8) === "vmess://") {
+            // Decode the vmess configuration
+            $the_config = decode_vmess($config);
 
-                // Extract IP and port from the decoded configuration
-                $ip = !empty($the_config["sni"])
-                    ? $the_config["sni"]
-                    : (!empty($the_config["host"])
-                        ? $the_config["host"]
-                        : $the_config["add"]);
-                $port = $the_config["port"];
+            // Extract IP and port from the decoded configuration
+            $ip = !empty($the_config["sni"])
+                ? $the_config["sni"]
+                : (!empty($the_config["host"])
+                    ? $the_config["host"]
+                    : $the_config["add"]);
+            $port = $the_config["port"];
 
-                // Ping the IP and port to get the response time
-                @$ping_data = ping($ip, $port);
+            // Ping the IP and port to get the response time
+            @$ping_data = ping($ip, $port);
 
-                // If ping data is available
-                if ($ping_data !== "unavailable") {
-                    // Get IP information (country) and flag
-                    $ip_info = ip_info($ip);
-                    if (isset($ip_info["country"])) {
-                        $location = $ip_info["country"];
-                        $flag = getFlags($location);
-                    } else {
-                        $flag = "🚩";
-                    }
+            // If ping data is available
+            if ($ping_data !== "unavailable") {
+                // Get IP information (country) and flag
+                $ip_info = ip_info($ip);
+                if (isset($ip_info["country"])) {
+                    $location = $ip_info["country"];
+                    $flag = getFlags($location);
+                } else {
+                    $flag = "🚩";
+                }
 
-                    // Update the configuration with channel info, flag, and ping data
-                    $the_config["ps"] =
-                        "@" . $channel . "|" . $flag . "|" . $ping_data;
+                // Update the configuration with channel info, flag, and ping data
+                $the_config["ps"] =
+                    "@" . $channel . "|" . $flag . "|" . $ping_data;
 
-                    // Encode the updated configuration
-                    $final_config = encode_vmess($the_config);
+                // Encode the updated configuration
+                $final_config = encode_vmess($the_config);
 
-                    // Build the final data array with channel, type, config, and time
-                    $final_data['vmess'][$array_helper_vmess]["channel"]['username'] = $channel;
-                    $final_data['vmess'][$array_helper_vmess]["channel"]['title'] = $channel;
-                    $final_data['vmess'][$array_helper_vmess]["channel"]['logo'] = "null";
-                    $final_data['vmess'][$array_helper_vmess]["type"] = "vmess";
-                    $final_data['vmess'][$array_helper_vmess]["config"] = $final_config;
-                    $final_data['vmess'][$array_helper_vmess]["time"] = tehran_time();
-                    $array_helper_vmess ++ ;
+                // Build the final data array with channel, type, config, and time
+                $final_data["vmess"][$array_helper_vmess]["channel"][
+                    "username"
+                ] = $channel;
+                $final_data["vmess"][$array_helper_vmess]["channel"][
+                    "title"
+                ] = $channel;
+                $final_data["vmess"][$array_helper_vmess]["channel"]["logo"] =
+                    "null";
+                $final_data["vmess"][$array_helper_vmess]["type"] = "vmess";
+                $final_data["vmess"][$array_helper_vmess][
+                    "config"
+                ] = $final_config;
+                $final_data["vmess"][$array_helper_vmess][
+                    "time"
+                ] = tehran_time();
+                $array_helper_vmess++;
             }
         } elseif (substr($config, 0, 8) === "vless://") {
             // Parse the vless configuration
-            $the_config = parseProxyUrl(
-                $config,
-                "vless"
-            );
+            $the_config = parseProxyUrl($config, "vless");
 
             // Extract IP and port from the parsed vless configuration
             $ip = !empty($the_config["params"]["sni"])
@@ -359,15 +385,24 @@ function process_subscription($input, $channel){
                 $final_config = buildProxyUrl($the_config, "vless");
 
                 // Build the final data array with channel, type, config, and time
-                $final_data['vless'][$array_helper_vless]["channel"]['username'] = $channel;
-                $final_data['vless'][$array_helper_vless]["channel"]['title'] = $channel;
-                $final_data['vless'][$array_helper_vless]["channel"]['logo'] = "null";
-                $final_data['vless'][$array_helper_vless]["type"] = $type;
-                $final_data['vless'][$array_helper_vless]["config"] = urldecode($final_config);
-                $final_data['vless'][$array_helper_vless]["time"] = tehran_time();
-                $array_helper_vless ++ ;
+                $final_data["vless"][$array_helper_vless]["channel"][
+                    "username"
+                ] = $channel;
+                $final_data["vless"][$array_helper_vless]["channel"][
+                    "title"
+                ] = $channel;
+                $final_data["vless"][$array_helper_vless]["channel"]["logo"] =
+                    "null";
+                $final_data["vless"][$array_helper_vless]["type"] = $type;
+                $final_data["vless"][$array_helper_vless]["config"] = urldecode(
+                    $final_config
+                );
+                $final_data["vless"][$array_helper_vless][
+                    "time"
+                ] = tehran_time();
+                $array_helper_vless++;
             }
-        } elseif(substr($config, 0, 5) === "ss://") {
+        } elseif (substr($config, 0, 5) === "ss://") {
             // Parse the shadowsocks configuration
             $the_config = ParseShadowsocks($config);
 
@@ -396,55 +431,71 @@ function process_subscription($input, $channel){
                 $final_config = BuildShadowsocks($the_config);
 
                 // Build the final data array with channel, type, config, and time
-                $final_data['ss'][$array_helper_ss]["channel"]['username'] = $channel;
-                $final_data['ss'][$array_helper_ss]["channel"]['title'] = $channel;
-                $final_data['ss'][$array_helper_ss]["channel"]['logo'] = "null";
-                $final_data['ss'][$array_helper_ss]["type"] = "ss";
-                $final_data['ss'][$array_helper_ss]["config"] = urldecode($final_config);
-                $final_data['ss'][$array_helper_ss]["time"] = tehran_time();
-                $array_helper_ss ++ ;
+                $final_data["ss"][$array_helper_ss]["channel"][
+                    "username"
+                ] = $channel;
+                $final_data["ss"][$array_helper_ss]["channel"][
+                    "title"
+                ] = $channel;
+                $final_data["ss"][$array_helper_ss]["channel"]["logo"] = "null";
+                $final_data["ss"][$array_helper_ss]["type"] = "ss";
+                $final_data["ss"][$array_helper_ss]["config"] = urldecode(
+                    $final_config
+                );
+                $final_data["ss"][$array_helper_ss]["time"] = tehran_time();
+                $array_helper_ss++;
             }
-        } elseif (stripos($config, "trojan://")){
-        // Parse the trojan configuration
-        $the_config = parseProxyUrl($config);
+        } elseif (stripos($config, "trojan://")) {
+            // Parse the trojan configuration
+            $the_config = parseProxyUrl($config);
 
-        // Extract IP and port from the parsed trojan configuration
-        $ip = !empty($the_config["params"]["sni"])
-            ? $the_config["params"]["sni"]
-            : (!empty($the_config["params"]["host"])
-                ? $the_config["params"]["host"]
-                : $the_config["hostname"]);
-        $port = $the_config["port"];
+            // Extract IP and port from the parsed trojan configuration
+            $ip = !empty($the_config["params"]["sni"])
+                ? $the_config["params"]["sni"]
+                : (!empty($the_config["params"]["host"])
+                    ? $the_config["params"]["host"]
+                    : $the_config["hostname"]);
+            $port = $the_config["port"];
 
-        // Ping the IP and port to get the response time
-        @$ping_data = ping($ip, $port);
+            // Ping the IP and port to get the response time
+            @$ping_data = ping($ip, $port);
 
-        // If ping data is available
-        if ($ping_data !== "unavailable") {
-            // Get IP information (country) and flag
-            $ip_info = ip_info($ip);
-            if (isset($ip_info["country"])) {
-                $location = $ip_info["country"];
-                $flag = getFlags($location);
-            } else {
-                $flag = "🚩";
+            // If ping data is available
+            if ($ping_data !== "unavailable") {
+                // Get IP information (country) and flag
+                $ip_info = ip_info($ip);
+                if (isset($ip_info["country"])) {
+                    $location = $ip_info["country"];
+                    $flag = getFlags($location);
+                } else {
+                    $flag = "🚩";
+                }
+
+                // Update the configuration with channel info, flag, and ping data
+                $the_config["hash"] =
+                    "@" . $channel . "|" . $flag . "|" . $ping_data;
+
+                // Build the final trojan configuration
+                $final_config = buildProxyUrl($the_config);
+
+                // Build the final data array with channel, type, config, and time
+                $final_data["trojan"][$array_helper_trojan][
+                    "username"
+                ] = $channel;
+                $final_data["trojan"][$array_helper_trojan]["channel"][
+                    "title"
+                ] = $channel;
+                $final_data["trojan"][$array_helper_trojan]["channel"]["logo"] =
+                    "null";
+                $final_data["trojan"][$array_helper_trojan]["type"] = "trojan";
+                $final_data["trojan"][$array_helper_trojan][
+                    "config"
+                ] = urldecode($final_config);
+                $final_data["trojan"][$array_helper_trojan][
+                    "time"
+                ] = tehran_time();
+                $array_helper_trojan++;
             }
-
-            // Update the configuration with channel info, flag, and ping data
-            $the_config["hash"] =
-                "@" . $channel . "|" . $flag . "|" . $ping_data;
-
-            // Build the final trojan configuration
-            $final_config = buildProxyUrl($the_config);
-
-            // Build the final data array with channel, type, config, and time
-            $final_data['trojan'][$array_helper_trojan]['username'] = $channel;
-            $final_data['trojan'][$array_helper_trojan]["channel"]['title'] = $channel;
-            $final_data['trojan'][$array_helper_trojan]["channel"]['logo'] = 'null';
-            $final_data['trojan'][$array_helper_trojan]["type"] = "trojan";
-            $final_data['trojan'][$array_helper_trojan]["config"] = urldecode($final_config);
-            $final_data['trojan'][$array_helper_trojan]["time"] = tehran_time();
-            $array_helper_trojan ++ ;
         }
     }
 }
