b7ecd6688855ce9c4dcf169f36b1d003209d4df5
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-19 23:36:58 +0330

    Update ranking.php

--- modules/ranking.php
+++ modules/ranking.php
@@ -30,33 +30,29 @@ function compare_time($a, $b)
     return $a_time > $b_time ? -1 : 1;
 }
 
-function ranking($input, $type)
-{
-    $last_point_array = [];
+function point_array($input){
     $usernames = array_column(array_column($input, "channel"), "username");
     $point_array = array_count_values($usernames);
-    $lowest_ping = array_reduce($input, function($min, $item) {
+    return $point_array;
+}
+
+function lowest_ping($input){
+    return array_reduce($input, function($min, $item) {
         return ($item['ping'] < $min) ? $item['ping'] : $min;
     }, PHP_FLOAT_MAX);
+}
+function time_coefficient($config_time){
+    $time_diff = strtotime(tehran_time()) - strtotime($config_time);
+    $time_coefficient = 1 / ($time_diff + 1);
+    return $time_coefficient;
+}
+function ping_coefficient($config_ping, $lowest_ping){
+    $ping_diff = $config_ping - $lowest_ping;
+    $ping_coefficient = 1 / ($ping_diff + 1);
+    return $ping_coefficient;
+}
 
-    foreach ($input as $key => $config){
-        
-        $username_ch = $config['channel']['username'];
-        $time_diff = strtotime(tehran_time()) - strtotime($config['time']);
-        $time_coefficient = 1 / ($time_diff + 1);
-        $point_array[$username_ch] += $time_coefficient;
-
-        $config_ping = $config['ping'];
-        $ping_diff = $config_ping - $lowest_ping;
-        $ping_coefficient = 1 / ($ping_diff + 1);
-        $point_array[$username_ch] += $ping_coefficient;
-        
-        switch ($config['type']){
-            case "reality":
-                $point_array[$username_ch] += 1 ;
-        }
-    }
-
+function process_rank_file($type){
     if (file_exists("ranking/channel_ranking_" . $type . ".json")) {
         $last_point_array = json_decode(
             file_get_contents("ranking/channel_ranking_" . $type . ".json"),
@@ -70,7 +66,10 @@ function ranking($input, $type)
     } else {
         $last_point_array['points'] = [];
     }
+    return $last_point_array;
+}
 
+function process_points($point_array, $last_point_array){
     if (!empty(array_diff_assoc($point_array, $last_point_array['points']))) {
         foreach ($point_array as $channel => $point) {
             if (array_key_exists($channel, $last_point_array['points'])) {
@@ -80,6 +79,34 @@ function ranking($input, $type)
             }
         }
     }
+    return $last_point_array;
+}
+
+function ranking($input, $type)
+{
+    $last_point_array = [];
+
+    $point_array = point_array($input);
+    $lowest_ping = lowest_ping($input);
+
+    foreach ($input as $key => $config){
+        
+        $username_ch = $config['channel']['username'];
+
+        $time_coefficient = time_coefficient($config['time']);
+        $point_array[$username_ch] += $time_coefficient;
+
+        $ping_coefficient = ping_coefficient($config['ping'], $lowest_ping);
+        $point_array[$username_ch] += $ping_coefficient;
+        
+        switch ($config['type']){
+            case "reality":
+                $point_array[$username_ch] += 1 ;
+                break;
+        }
+    }
+    
+    $last_point_array = process_points($point_array, process_rank_file($type));
 
     $final_point_array = [
         "date" => tehran_time(),
