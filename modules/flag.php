30c969f92b6753a3d9449637a38cf8bcbfd103bf
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-19 03:30:05 +0330

    Update flag.php

--- modules/flag.php
+++ modules/flag.php
@@ -1,7 +1,6 @@
 <?php
 function getFlags($country_code)
 {
-    $flag = "";
     $flag = mb_convert_encoding( '&#' . ( 127397 + ord( $country_code[0] ) ) . ';', 'UTF-8', 'HTML-ENTITIES');
     $flag .= mb_convert_encoding( '&#' . ( 127397 + ord( $country_code[1] ) ) . ';', 'UTF-8', 'HTML-ENTITIES');
     return $flag;
