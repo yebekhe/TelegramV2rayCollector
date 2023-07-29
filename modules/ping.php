c43f0529e0a749ec3e160d42e773170da667dc60
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-29 22:22:55 +0330

    Update ping.php

--- modules/ping.php
+++ modules/ping.php
@@ -4,7 +4,7 @@ function ping($ip, $port)
     $it = microtime(true);
     $check = @fsockopen($ip, $port, $errno, $errstr, 0.5);
     $ft = microtime(true);
-    $militime = round(($ft - $it) * 1e3, 4);
+    $militime = round(($ft - $it) * 1e3, 2);
     if ($check) {
         fclose($check);
         return $militime;
