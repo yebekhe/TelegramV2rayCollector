8e8b9ffc9ad5c03203c127b23a7bda83272355f0
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-08-05 18:56:17 +0330

    Update config.php

--- modules/config.php
+++ modules/config.php
@@ -95,7 +95,6 @@ $Types = [
     "VpnFreeSec" => ["vless"],
     "lightning6" => ["vless"],
     "WebShecan" => ["vless"],
-    "fv2ray" => ["vless"],
     "vpntwitt" => ["vless"],
     "v2Line" => ["vmess"],
     "vmessiran" => ["vmess", "vless"],
