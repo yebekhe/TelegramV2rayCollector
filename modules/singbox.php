a10549acf05cb59b7d3b77f6f1c51fa81454af00
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-08-07 16:06:26 +0330

    Update singbox.php

--- modules/singbox.php
+++ modules/singbox.php
@@ -144,10 +144,11 @@ function generate_output($input, $output){
                 preg_match_all($pattern, $the_name, $matches);
                 $server_location = $matches[0][0];
             }
-            $first_outbound[$server_location][] = json_decode($json_output, true);
+            if (!in_array(json_decode($json_output, true), $outbound[$server_location])) {
+                $outbound[$server_location][] = json_decode($json_output, true);
+            }
         }
     }
-    $outbound = deduplicate_singbox($first_outbound);
     $json_map = ["nekobox_old" => "nekobox_1.1.7.json", "nekobox_new" => "nekobox_1.1.8.json", "sfi" => "sfi.json"];
     $template = json_decode(file_get_contents("modules/singbox/" . $json_map[$output]), true);
     $manual_json = json_decode(file_get_contents("modules/singbox/manual.json"), true);
