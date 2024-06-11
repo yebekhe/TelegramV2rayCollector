2225f57a5cd4757dfaf4e43d540d285724f69806
NekoHanaku <94982112+NekoHanaku@users.noreply.github.com>
2024-06-11 05:21:14 +0330

    fix config file again to fix conflicts

--- modules/config.php
+++ modules/config.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("Asia/Tehran");
 
-$Types = json_decode(file_get_contents("modules/channels.json"), true);
+$Types = json_decode(file_get_contents("https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/modules/channels.json"), true);
 
 $donated_subscription = [
     "https://yebekhe.000webhostapp.com/donate/donated_servers/donated_server.json"
